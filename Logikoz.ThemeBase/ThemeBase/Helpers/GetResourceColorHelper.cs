4338842ebdf5df5efcd613f335b4dc78e1e0f353
Ruan Carlos CS <ruancarlos14@hotmail.com>
2020-01-06 13:47:40 -0500

    Marcando metodo como static.

--- Logikoz.ThemeBase/ThemeBase/Helpers/GetResourceColorHelper.cs
+++ Logikoz.ThemeBase/ThemeBase/Helpers/GetResourceColorHelper.cs
@@ -6,7 +6,10 @@ namespace Logikoz.ThemeBase.Helpers
 {
     public class GetResourceColorHelper
     {
-        public (Color? color, bool result) GetResourceColor(ColorsEnum resource)
+        /// <summary>
+        /// Obtem a <see cref="Color"/> de um determinado resource.
+        /// </summary>
+        public static (Color? color, bool result) GetResourceColor(ColorsEnum resource)
         {
             if (Application.Current.Resources.TryGetValue(resource.ToString(), out object color))
                 return ((Color)color, true);
