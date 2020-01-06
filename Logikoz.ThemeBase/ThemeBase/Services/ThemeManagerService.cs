0bd88883a30cf992b399b9ad0a53a56a215305ac
Ruan Carlos CS <ruancarlos14@hotmail.com>
2020-01-06 01:42:08 -0500

    Alterando cor do dark background.

--- Logikoz.ThemeBase/ThemeBase/Services/ThemeManagerService.cs
+++ Logikoz.ThemeBase/ThemeBase/Services/ThemeManagerService.cs
@@ -11,14 +11,14 @@ namespace Logikoz.ThemeBase.Services
 {
     public class ThemeManagerService
     {
-        public static void ChangeTheme(ThemeEnum theme)
+        public static void ChangeTheme(ThemeEnum theme, string key = "SelectedTheme")
         {
             ICollection<ResourceDictionary> mergedDictionaries = Application.Current.Resources.MergedDictionaries;
             if (mergedDictionaries != null)
             {
                 mergedDictionaries.Clear();
 
-                CrossSettings.Current.AddOrUpdateValue("SelectedTheme", (int)theme);
+                CrossSettings.Current.AddOrUpdateValue(key, (int)theme);
 
                 mergedDictionaries.Add(
                     theme switch
@@ -32,6 +32,6 @@ namespace Logikoz.ThemeBase.Services
 
         public static void LoadTheme() => ChangeTheme(CurrentTheme());
 
-        public static ThemeEnum CurrentTheme() => (ThemeEnum)CrossSettings.Current.GetValueOrDefault("SelectedTheme", (int)ThemeEnum.Light);
+        public static ThemeEnum CurrentTheme(string key = "SelectedTheme") => (ThemeEnum)CrossSettings.Current.GetValueOrDefault(key, (int)ThemeEnum.Light);
     }
 }
